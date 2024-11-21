13048591faa59364bfa79dc6292b3683ad378d39
Maxwell King <maxwellzking@berkeley.edu>
2024-11-21 14:31:36 -0800

    Adjusting Control Panels
    Made Multiple-Choice buttons, Made Sound masking A singular On/Off Switch,  Centered the Controls on the left side, Centered the mini map on the right side, Fixed the mini map grid layout, added a drop shadow to the header ribbon.

--- Test/script.js
+++ Test/script.js
@@ -1,15 +1,4 @@
-let isPlaying = false; // Track the state of the toggle
-
-function toggleImage() {
-    const toggleImage = document.getElementById('toggleImage');
-
-    if (isPlaying) {
-        toggleImage.src = 'Photos/Play Button.png'; // Change to your "Play" image path
-        toggleImage.alt = 'Pause Image';
-    } else {
-        toggleImage.src = 'Photos/Pause Button.png'; // Change to your "Pause" image path
-        toggleImage.alt = 'Play Image';
-    }
-
-    isPlaying = !isPlaying; // Toggle the state
+function updateSelected() {
+    const selectedOption = document.querySelector('input[name="options"]:checked');
+    const display = document.getElementById("selected-option");
 }
